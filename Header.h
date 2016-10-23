f274192015102fc1d7421d43164010eb8e48a73d
aditi741997 <aditi741997@gmail.com>
2016-10-24 01:09:23 +0530

    header ready! :p

---
+++ Header.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <>
+
+namespace Types
+{
+	typedef short int State;
+	typedef char Action;
+}
+
+// everything assuming N <= 5, K = 2.
+extern int N;
+extern int K;
+
+using namespace std;
+using namespace Types;
